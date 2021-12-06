78e9227865fcbe93bef99f3574a4a8e992030412
soaelee <soae0923@gmail.com>
2021-12-06 15:51:38 +0900

    Adding About Page and Next Link Component

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About </h1>
+    </main>
+  );
+};
+
+export default AboutPage;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   return(
+  <>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
     <main>
       <h1> My Blog </h1>
     </main>
+  </>
   )
 }
 
