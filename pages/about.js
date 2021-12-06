10665691b4fc8bd3d693159d3a126ad305c55a34
soaelee <soae0923@gmail.com>
2021-12-06 17:10:14 +0900

    Head Component

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,14 @@
+import Head from 'next/head';
+
 const AboutPage = () => {
   return (
+    <>
+      <Head>
+        <title>About - My Blog</title>
+        <meta name="description" value="This is about page" />
+      </Head>
       <h1> About </h1>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Head from 'next/head';
 
 const HomePage = () => {
   return(
+    <>
+      <Head>
+        <title>My Blog</title>
+        <meta name="description" value="This is my blog" />
+      </Head>
       <h1> My Blog </h1>
+    </>
   )
 }
 
